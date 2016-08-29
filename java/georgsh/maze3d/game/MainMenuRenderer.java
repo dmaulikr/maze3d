ed69c9032083cce93785c4dad9c80124d8148f31
georgsh <jura913@ya.ru>
2016-08-29 13:09:19 +0300

    add menu class as renderable object

---
+++ java/georgsh/maze3d/game/MainMenuRenderer.java
@@ -0,0 +1,35 @@
+package georgsh.maze3d.game;
+
+import android.opengl.GLSurfaceView.Renderer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by yura on 29.08.16.
+ */
+public class MainMenuRenderer implements Renderer
+    {
+    MainMenuRenderer()
+        {
+        
+        }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig)
+        {
+
+        }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int i, int i1)
+        {
+
+        }
+
+    @Override
+    public void onDrawFrame(GL10 gl10)
+        {
+
+        }
+    }
